40040efca5c3b3bd9b3efe13adb455cf101b476d
Thomas Leskin <tleskin@gmail.com>
2015-09-03 22:48:06 -0600

    Made AJAX call to OpenWeatherMap API, display data

--- src/app.js
+++ src/app.js
@@ -17,3 +17,25 @@ var ajax = require('ajax');
 
 var cityName = 'Boulder';
 var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+
+ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    console.log('Successfully fetched weather data!');
+    
+    var location = data.name;
+    var temperature = Math.round(data.main.temp - 273.15) + 'C';
+    
+    var description = data.weather[0].description;
+    description = description.charAt(0).toUpperCase() + description.substring(1);
+    
+    card.subtitle(location + ', ' + temperature);
+    card.body(description);
+  },
+  function(error) {
+    console.log('Failed fetching weather data: ' + error);
+  }
+);
